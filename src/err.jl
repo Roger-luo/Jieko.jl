bce5083bd92e4626e300c89fa27d46971946340f
Roger-luo <rogerluo.rl18@gmail.com>
2024-03-25 14:48:57 -0400

    add doc string err

--- src/err.jl
+++ src/err.jl
@@ -2,4 +2,9 @@
 
 struct NotImplementedError <: Exception end
 Base.showerror(io::IO, e::NotImplementedError) = print(io, "Not implemented yet")
+
+"""
+Raise a `NotImplementedError` exception. This is useful to mark
+an interface method as not implemented yet.
+"""
 not_implemented_error() = throw(NotImplementedError())
